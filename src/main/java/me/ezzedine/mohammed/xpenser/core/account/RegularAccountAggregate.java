12afb3fe3fc3818f3ea5d83a039689255cd33427
Mohammed Ezzedine <mohammed.a.ezzedine@outlook.com>
2024-06-22 12:00:28 +0300

    fix regular account command handler constructor to listen on the correct command

--- src/main/java/me/ezzedine/mohammed/xpenser/core/account/RegularAccountAggregate.java
+++ src/main/java/me/ezzedine/mohammed/xpenser/core/account/RegularAccountAggregate.java
@@ -6,7 +6,7 @@ import lombok.NoArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import me.ezzedine.mohammed.xpenser.core.account.budget.Budget;
 import me.ezzedine.mohammed.xpenser.core.account.opening.AccountOpenedEvent;
-import me.ezzedine.mohammed.xpenser.core.account.opening.OpenSavingsAccountCommand;
+import me.ezzedine.mohammed.xpenser.core.account.opening.OpenRegularAccountCommand;
 import org.axonframework.commandhandling.CommandHandler;
 import org.axonframework.spring.stereotype.Aggregate;
 
@@ -22,7 +22,7 @@ import static org.axonframework.modelling.command.AggregateLifecycle.apply;
 public class RegularAccountAggregate extends AccountAggregate {
 
     @CommandHandler
-    public RegularAccountAggregate(OpenSavingsAccountCommand command) {
+    public RegularAccountAggregate(OpenRegularAccountCommand command) {
         if (command.budgetInitialAmount().compareTo(BigDecimal.ZERO) < 0) {
             throw new IllegalArgumentException("Amount cannot be negative.");
         }
